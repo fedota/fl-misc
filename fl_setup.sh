672cded5fe09bcd83bb0f4068cb9e536fb8f7c50
Shashank P <1997shashankp@gmail.com>
2019-10-16 22:24:49 +0530

    Introduced selector

--- fl_setup.sh
+++ fl_setup.sh
@@ -4,12 +4,14 @@ set -e
 server=https://github.com/omkarprabhu-98/fl-server
 client=https://github.com/pvgupta24/fl-client
 proto=https://github.com/pvgupta24/fl-proto
+selector=https://github.com/ShashankP19/fl-selector
 
 cd ..
 
 git clone $server
 git clone $client
 git clone $proto
+git clone $selector
 
 cd fl-misc
 bash fl_proto.sh
