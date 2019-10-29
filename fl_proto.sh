eb23651ee421fd71cbe08feb0171ca6159b1f0f2
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2019-10-29 13:44:02 +0530

    instructions updated

--- fl_proto.sh
+++ fl_proto.sh
@@ -5,6 +5,7 @@ protodir=../fl-proto
 client_gen=../fl-client
 server_gen=../fl-server/genproto
 selector_gen=../fl-selector/genproto
+coordinator_gen=../fl-coordinator/genproto
 
 python3 -m grpc_tools.protoc -I=$protodir --python_out=$client_gen --grpc_python_out=$client_gen $protodir/fl_round/fl_round.proto
 
@@ -19,3 +20,10 @@ if [ ! -d $selector_gen ]; then
 fi
 
 protoc --go_out=plugins=grpc:$selector_gen -I $protodir $protodir/fl_intra/fl_intra.proto
+protoc --go_out=plugins=grpc:$selector_gen -I $protodir $protodir/fl_round/fl_round.proto
+
+
+if [ ! -d $coordinator_gen ]; then
+    mkdir $coordinator_gen;
+fi
+protoc --go_out=plugins=grpc:$coordinator_gen -I $protodir $protodir/fl_intra/fl_intra.proto
--- fl_setup.sh
+++ fl_setup.sh
@@ -5,6 +5,7 @@ server=https://github.com/omkarprabhu-98/fl-server
 client=https://github.com/pvgupta24/fl-client
 proto=https://github.com/pvgupta24/fl-proto
 selector=https://github.com/ShashankP19/fl-selector
+coordinator=https://github.com/ShashankP19/fl-coordinator
 
 cd ..
 
@@ -12,6 +13,7 @@ git clone $server
 git clone $client
 git clone $proto
 git clone $selector
+git clone $coordinator
 
 cd fl-misc
 bash fl_proto.sh
