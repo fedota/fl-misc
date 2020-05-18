3854b4fec14a638fb2f6e6f9a6091f97215612d3
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2020-05-18 23:17:34 +0530

    updates and instructions

--- fl_proto.sh
+++ fl_proto.sh
@@ -3,6 +3,7 @@ set -e
 
 protodir=../fl-proto
 client_gen=../fl-client
+webserver_gen=../fl-webserver
 server_gen=../fl-server/genproto
 selector_gen=../fl-selector/genproto
 coordinator_gen=../fl-coordinator/genproto
@@ -13,18 +14,8 @@ selector_gen=../fl-selector/genproto
 echo "Compiling to $client_gen"
 python3 -m grpc_tools.protoc -I=$protodir --python_out=$client_gen --grpc_python_out=$client_gen $protodir/fl_round/fl_round.proto
 
-if [ ! -d $server_gen ]; then
-    mkdir $server_gen;
-fi
-# echo "Compiling to $server_gen"
-# protoc --go_out=plugins=grpc:$server_gen -I $protodir $protodir/fl_round/fl_round.proto
-
-
-if [ ! -d $coordinator_gen ]; then
-    mkdir $coordinator_gen;
-fi
-echo "Compiling to $coordinator_gen"
-protoc --go_out=plugins=grpc:$coordinator_gen -I $protodir $protodir/fl_intra/fl_intra.proto
+echo "Compiling to $webserver_gen"
+python3 -m grpc_tools.protoc -I=$protodir --python_out=$webserver_gen --grpc_python_out=$webserver_gen $protodir/fl_status/fl_status.proto
 
 if [ ! -d $selector_gen ]; then
     mkdir $selector_gen;
@@ -32,3 +23,10 @@ fi
 echo "Compiling to $selector_gen"
 protoc --go_out=plugins=grpc:$selector_gen -I $protodir $protodir/fl_intra/fl_intra.proto
 protoc --go_out=plugins=grpc:$selector_gen -I $protodir $protodir/fl_round/fl_round.proto
+
+if [ ! -d $coordinator_gen ]; then
+    mkdir $coordinator_gen;
+fi
+echo "Compiling to $coordinator_gen"
+protoc --go_out=plugins=grpc:$coordinator_gen -I $protodir $protodir/fl_intra/fl_intra.proto
+protoc --go_out=plugins=grpc:$coordinator_gen -I $protodir $protodir/fl_status/fl_status.proto
--- fl_setup.sh
+++ fl_setup.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 set -e
 
-server=https://github.com/omkarprabhu-98/fl-server
-client=https://github.com/pvgupta24/fl-client
-proto=https://github.com/pvgupta24/fl-proto
-selector=https://github.com/ShashankP19/fl-selector
-coordinator=https://github.com/ShashankP19/fl-coordinator
+# server=https://github.com/fedota/fl-server
+client=https://github.com/fedota/fl-client
+proto=https://github.com/fedota/fl-proto
+selector=https://github.com/fedota/fl-selector
+coordinator=https://github.com/fedota/fl-coordinator
+webserver=https://github.com/fedota/fl-webserver
+fedotaInfra=https://github.com/fedota/fedota-infra
 
 cd ..
 
-git clone $server
+# git clone $server
 git clone $client
 git clone $proto
 git clone $selector
 git clone $coordinator
+git clone $webserver
+git clone $fedotaInfra
 
 cd fl-misc
 bash fl_proto.sh
